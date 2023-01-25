c090e8099661f4dbb479aeb26608ecf63f8b8ee1
sahujyoti50 <sahujyoti50@gmail.com>
2023-01-25 12:19:26 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import Validation from './Validation/Validation';
+
+const App = () => {
+  return (
+    <div>
+      <Validation />
+    </div>
+  )
+}
+export default App;
